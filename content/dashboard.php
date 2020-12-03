fe52a07b026ea96c5a0ffdc50dd06240608be932
ikhsanmoh <icanmohamad13@gmail.com>
2020-12-03 15:51:23 +0700

    Penambahan api untuk baca semua data payment dari database dan api untuk menginput data payment dari URL. Menampilkan Level User pada halaman dashboard

--- content/dashboard.php
+++ content/dashboard.php
@@ -2,5 +2,6 @@
    if(!defined('INDEX')) die("");
 ?>
 
+
 <h1>Selamat Datang di Aplikasi Manajemen Keuangan</h1>
-<h3>Anda login sebagai Administrator</h3>
+<h3>Anda login sebagai <?= ucfirst($level) ?> </h3>
